f1111355c538e03bda03a07d6d4913e752a0dace
Umer <md.farooq.ansari8959149@gmail.com>
2024-09-29 02:14:19 +0500

    schema has created

---
+++ src/domain/models/userPreferences.ts
@@ -0,0 +1,34 @@
+import mongoose, { Schema, Document, model } from 'mongoose';
+import { IUserPref } from '../../presentation/interfaces/Mongo_Schemas_Interfaces/UserPreferences';
+
+
+// Define the Mongoose schema
+const UserPrefSchema: Schema<IUserPref> = new Schema<IUserPref>(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      required: false,
+    },
+    name: {
+      type: String, 
+      required: false,
+    },
+    type: {
+      type: String, 
+      required: false,
+    },
+    value: {
+      type: String, 
+      required: false,
+    },
+  },
+  {
+    timestamps: true, 
+    strict: true,     
+  }
+);
+
+
+const UserPreferences = model<IUserPref>('UserPreferences', UserPrefSchema);
+
+export default UserPreferences;
---
+++ src/presentation/interfaces/Mongo_Schemas_Interfaces/UserPreferences.ts
@@ -0,0 +1,8 @@
+import mongoose from "mongoose";
+
+export interface IUserPref extends Document {
+    userId: mongoose.Types.ObjectId | string; // Allow both ObjectId or string
+    name: string;
+    type: string;
+    value: string;
+  }
